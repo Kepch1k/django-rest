95d17f84bc5a2ce80e4dfdea20295e05fc81279b
Alexander Osnova <kephik2000@gmail.com>
2022-07-19 12:25:48 +0300

    add STATIC_ROOT to settings.py

--- djangoreactproject/settings.py
+++ djangoreactproject/settings.py
@@ -146,6 +146,8 @@ STATICFILES_DIRS = [
     BUILD_PATH / 'static',
 ]
 
+STATIC_ROOT = Path(BASE_DIR).resolve().joinpath('staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
